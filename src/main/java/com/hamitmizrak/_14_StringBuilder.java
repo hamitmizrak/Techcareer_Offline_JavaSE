825cc489b46a01f081e1756ddc8dae65ba36768f
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-15 20:58:27 +0300

    StringBuilder

---
+++ src/main/java/com/hamitmizrak/_14_StringBuilder.java
@@ -0,0 +1,24 @@
+package com.hamitmizrak;
+
+public class _14_StringBuilder {
+
+    public static void main(String[] args) {
+
+        String tech1="java",tech2="jsp",tech3="servlet";
+
+        // + operant
+        String allData = tech1 + tech2 + tech3;
+        System.out.println(allData);
+
+        // String concat
+        String concatString = tech1.concat(tech2).concat(tech3);
+        System.out.println(concatString);
+
+       // StringBuilder
+        StringBuilder stringBuilder= new StringBuilder(tech1);
+        stringBuilder.append(tech2).append(tech3);
+        String toChange=stringBuilder.toString();
+        System.out.println(toChange);
+
+    }
+}
