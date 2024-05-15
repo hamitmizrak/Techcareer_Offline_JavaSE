fc13e7c76fcf10fce52e6e234e452dd3bf9822d8
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-15 13:34:43 +0300

    Sayı Sistemi

---
+++ src/main/java/com/hamitmizrak/_6_NumbersSystem.java
@@ -0,0 +1,23 @@
+package com.hamitmizrak;
+
+public class _6_NumbersSystem {
+    public static void main(String[] args) {
+        System.out.println("Sayı Sistemleri");
+
+        // Binary: 0-1
+        int binary=0b0011100;
+        System.out.println(binary);
+
+        // Octal: 0-1-2-3-4-5-6-7
+        int octal=0777;
+        System.out.println(octal);
+
+        // Decimal: 0-1-2-3-4-5-6-7-8-9
+        int decimal=123456;
+        System.out.println(decimal);
+
+        // Hexadecimal: 0-1-2-3-4-5-6-7-8-9-a-b-c-d-e-f
+        int hexadecimal=0xabc12345;
+        System.out.println(hexadecimal);
+    }
+}
