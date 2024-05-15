708a4a6ce12d630cb7cb3a48a3d99ebf6addcbaf
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-15 20:12:32 +0300

    Math

---
+++ src/main/java/com/hamitmizrak/_11_Math.java
@@ -0,0 +1,40 @@
+package com.hamitmizrak;
+
+
+public class _11_Math {
+
+    public static void main(String[] args) {
+       // Mathematics (Math)
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        System.out.println(Math.min(10,99));
+        System.out.println(Math.max(10,99));
+        System.out.println(Math.abs(-25));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.pow(3,5));
+
+        System.out.println(Math.sin(Math.PI/6));
+        System.out.println(Math.cos(Math.PI/3));
+        System.out.println(Math.tan(Math.PI/4));
+
+
+        System.out.println(Math.floor(3.9));
+        System.out.println(Math.ceil(3.1));
+        System.out.println(Math.round(5.4));
+        System.out.println(Math.round(5.5));
+
+
+        System.out.println(Math.pow(2,(Math.min(Math.abs(-3),5))));
+
+
+
+
+
+
+
+
+
+
+    }
+}
