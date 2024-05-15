81bb68971bc285718bddca48a64fad89a0104756
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-15 13:38:30 +0300

    jOptionalPane

---
+++ src/main/java/com/hamitmizrak/_8_JOptionalPane.java
@@ -0,0 +1,17 @@
+package com.hamitmizrak;
+
+import javax.swing.*;
+
+public class _8_JOptionalPane {
+    public static void main(String[] args) {
+        System.out.println("jOptionalPane");
+
+        // Ekranda Göstermek
+        JOptionPane.showMessageDialog(null, "Nasilsiniz");
+
+        // Kullanıcıdan Veri almak
+        String name = JOptionPane.showInputDialog("Lütfen ismizi giriniz");
+        JOptionPane.showMessageDialog(null, "Adiniz:" + name);
+
+    }
+}
