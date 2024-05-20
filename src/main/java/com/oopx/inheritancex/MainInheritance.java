e62afff2ac4bb34edb1d945195c756cf9a5c1556
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-20 14:14:41 +0300

    Inheritance

--- src/main/java/com/oopx/inheritancex/MainInheritance.java
+++ src/main/java/com/oopx/inheritancex/MainInheritance.java
@@ -4,19 +4,16 @@ public class MainInheritance {
 
     public static void main(String[] args) {
 
-        // Student
+        // Person
         Person person=new Person("person adı-1","person soyadı-1");
         person.fullName();
         System.out.println(person);
-
         System.out.println("**********************************");
 
         // Student
         Student student=new Student("öğrenci adı-1","öğrenci soyadı-1","öğrenciye özel");
         student.fullName();
-
         System.out.println(student);
-
         System.out.println("**********************************");
 
         // Student
